95a9be4a92c3b9a23b8957158744c12c2fceed14
RuslanBisembaev <rusl.bisembaev@gmail.com>
2022-06-22 14:34:26 +0200

    fix delete mapping

--- src/main/java/de/htwberlin/webtech/einundzwanzig/web/UserRestController.java
+++ src/main/java/de/htwberlin/webtech/einundzwanzig/web/UserRestController.java
@@ -50,7 +50,7 @@ public class UserRestController {
         return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
-    @DeleteMapping(path = "/api/v1/users{id}")
+    @DeleteMapping(path = "/api/v1/users/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         boolean successful = userService.deleteById(id);
         return successful ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
